c675e35b118b6a3d6d11863fcfdafb73045b3c7b
ClaKaJe <clakajedev@gmail.com>
2024-06-06 10:03:22 +0100

    sql file update

--- food_db2.sql
+++ food_db2.sql
@@ -1,85 +1,92 @@
--- Active: 1716527985679@@127.0.0.1@3306@food_db2
+-- Active: 1716527985679@@127.0.0.1@3306@food_db
 CREATE TABLE `admin` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(20),
-  `password` varchar(50)
-);
-
-CREATE TABLE `cart` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  FOREIGN KEY `user_id` REFERENCES 'users' ('id'),
-  `name` varchar(100),
-  `price` int(10),
-  `quantity` int(10),
-  `image` varchar(100)
-);
-
-CREATE TABLE `messages` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int(100),
-  `message` varchar(500)
-);
-
-CREATE TABLE `orders` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int(100),
-  `payement_method_id` int(100),
-  `total_products` varchar(1000),
-  `total_price` int(100),
-  `placed_on` date,
-  `payment_status` varchar(20)
-);
-
-CREATE TABLE `products` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `admin_id` int(100),
-  `name` varchar(100),
-  `price` int(10),
-  `image` varchar(100),
-  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `name` varchar(20) NOT NULL,
+    `password` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
 CREATE TABLE `address` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `flat_no` int(10),
-  `building_no` int(10),
-  `area_name` varchar(45),
-  `city_name` varchar(45),
-  `state_name` varchar(45),
-  `country_name` varchar(45),
-  `pin_code` int(10)
-);
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `flat_no` int(10) DEFAULT NULL,
+    `building_no` int(10) DEFAULT NULL,
+    `area_name` varchar(45) NOT NULL,
+    `city_name` varchar(45) NOT NULL,
+    `postal_code` int(10) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
 CREATE TABLE `users` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `address_id` int(100),
-  `name` varchar(20),
-  `email` varchar(50),
-  `number` varchar(10),
-  `password` varchar(50)
-);
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `address_id` int(100) NOT NULL,
+    `name` varchar(20) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `number` varchar(20) NOT NULL,
+    `password` varchar(50) NOT NULL,
+    `registered_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    PRIMARY KEY (`id`),
+    KEY `address_id` (`address_id`),
+    CONSTRAINT `users_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `address` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
 CREATE TABLE `categories` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `products_id` int(100),
-  `name` varchar(50)
-);
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-CREATE TABLE `payement_method` (
-  `id` int(100) UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `name` varchar(45)
-);
+CREATE TABLE `products` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `name` varchar(100) NOT NULL,
+    `price` int(10) NOT NULL,
+    `image` varchar(100) NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+    `description` varchar(500) DEFAULT 'NULL',
+    `categories_id` int(100) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `categories_id` (`categories_id`),
+    CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-ALTER TABLE `cart` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+CREATE TABLE `cart` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `user_id` int(100) NOT NULL,
+    `name` varchar(100) NOT NULL,
+    `price` int(10) NOT NULL,
+    `quantity` int(10) NOT NULL,
+    `image` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 15 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-ALTER TABLE `messages` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+CREATE TABLE `messages` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `user_id` int(100) NOT NULL,
+    `message` varchar(500) NOT NULL,
+    `created` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    PRIMARY KEY (`id`),
+    KEY `user_id` (`user_id`),
+    CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 6 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-ALTER TABLE `orders` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+CREATE TABLE `payment_method` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `name` varchar(45) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
 
-ALTER TABLE `products` ADD FOREIGN KEY (`admin_id`) REFERENCES `admin` (`id`);
-
-ALTER TABLE `categories` ADD FOREIGN KEY (`products_id`) REFERENCES `products` (`id`);
-
-ALTER TABLE `users` ADD FOREIGN KEY (`address_id`) REFERENCES `address` (`id`);
-
-ALTER TABLE `orders` ADD FOREIGN KEY (`payement_method_id`) REFERENCES `payement_method` (`id`);
+CREATE TABLE `orders` (
+    `id` int(100) NOT NULL AUTO_INCREMENT,
+    `user_id` int(100) NOT NULL,
+    `payment_method_id` int(100) NOT NULL,
+    `total_products` varchar(1000) NOT NULL,
+    `total_price` int(100) NOT NULL,
+    `placed_on` timestamp NOT NULL DEFAULT current_timestamp(),
+    `payment_status` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `user_id` (`user_id`),
+    KEY `payement_method_id` (`payment_method_id`),
+    CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_method` (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 8 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci
